855f5f3e285615b91df4edfbaca45239df6bdccb
KID <50666207+KID421@users.noreply.github.com>
2020-08-15 19:47:14 +0800

    認識迴圈與協程

---
+++ RPG/Assets/Scripts/LearnCoroutine.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;   // 使用協程需要此 API
+
+public class LearnCoroutine : MonoBehaviour
+{
+    public Transform ming;
+
+    private void Start()
+    {
+        // 啟動協程
+        StartCoroutine(Test());
+        StartCoroutine(Big());
+    }
+
+    // 定義協程
+    // 傳回類型必須是 IEnumerator 傳回時間
+    public IEnumerator Test()
+    {
+        print("嗨，我是協程");
+        yield return new WaitForSeconds(2);     // 等待時間
+        print("嗨，我是過了兩秒的協程");
+        yield return new WaitForSeconds(1);     // 等待時間
+        print("又過了一秒!!!");
+    }
+
+    public IEnumerator Big()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            ming.localScale += Vector3.one;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
---
+++ RPG/Assets/Scripts/LearnLoop.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class LearnLoop : MonoBehaviour
+{
+    public int i = 1;
+
+    // 要生成的立方體
+    public Transform cube;
+
+    private void Start()
+    {
+        // 當布林值為 true 時執行一次
+        if (true)
+        {
+            print("我是判斷式");
+        }
+        // 迴圈：重複執行相同的程式
+        // 迴圈：while
+        // 當布林值為 true 時持續執行
+        while (i < 6)
+        {
+            print("我是迴圈 while " + i);
+            i++;
+        }
+
+        // 迴圈：for
+        for (int i = 1; i < 6; i++)
+        {
+            print("我是迴圈 for " + i);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 pos = new Vector3(i, 0, 0);
+            // 生成(物件，座標，角度)
+            // Quaternion.identity 零角度
+            Instantiate(cube, pos, Quaternion.identity);
+        }
+    }
+}
