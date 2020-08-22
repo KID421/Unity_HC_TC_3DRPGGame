3f2cc3af54378e3329b9b1510bd7835f95203601
KID <50666207+KID421@users.noreply.github.com>
2020-08-22 20:15:07 +0800

    完成移動與攝影機

--- RPG/Assets/Scripts/Player.cs
+++ RPG/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour
     #region 欄位
     [Header("速度"), Range(0, 1000)]
     public float speed = 1;
+    [Header("旋轉速度"), Range(0, 1000)]
+    public float turn = 60;
 
     private float attack = 10;
     private float hp = 100;
@@ -14,6 +16,7 @@ public class Player : MonoBehaviour
 
     private Rigidbody rig;
     private Animator ani;
+    private Transform cam;  // 攝影機根物件
     #endregion
 
     #region 事件
@@ -23,6 +26,7 @@ public class Player : MonoBehaviour
         // 泛型：所有類型
         rig = GetComponent<Rigidbody>();
         ani = GetComponent<Animator>();
+        cam = GameObject.Find("攝影機根物件").transform;
     }
 
     private void FixedUpdate()
@@ -39,10 +43,17 @@ public class Player : MonoBehaviour
     {
         float v = Input.GetAxis("Vertical");                            // 前後：WS 上下
         float h = Input.GetAxis("Horizontal");                          // 左右：AD 左右
-        Vector3 pos = transform.forward * v + transform.right * h;      // 移動座標 = 角色.前方 * 前後 + 角色.右方 * 左右
+        Vector3 pos = cam.forward * v + cam.right * h;                  // 移動座標 = 攝影機.前方 * 前後 + 攝影機.右方 * 左右
         rig.MovePosition(transform.position + pos * speed);             // 移動座標(原本座標 + 移動座標)
 
         ani.SetFloat("移動", Mathf.Abs(v) + Mathf.Abs(h));               // 設定浮點數(絕對值 v 與 h)
+
+        if (v != 0 || h != 0)                                                           // 如果 控制中
+        {
+            pos.y = 0;                                                                  // 移動座標.y = 0
+            Quaternion angle = Quaternion.LookRotation(pos);                            // B 角度 = 面向(移動座標)
+            transform.rotation = Quaternion.Slerp(transform.rotation, angle, turn);     // A 角度 = 角度.插值(A 角度，B 角度，旋轉速度)
+        }
     }
 
     private void Attack()
