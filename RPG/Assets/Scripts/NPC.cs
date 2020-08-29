e46b020575899eac34e382bca1d5f21ffd55fa9f
KID <50666207+KID421@users.noreply.github.com>
2020-08-29 14:48:33 +0800

    NPC 資料

---
+++ RPG/Assets/Scripts/NPC.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCData data;
+
+    /// <summary>
+    /// 對話系統
+    /// </summary>
+    public void Dialog()
+    {
+
+    }
+}
